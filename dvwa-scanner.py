84871bae7a93829258ac00d4e62d02a7d948112a
dsoda86 <122141740+dsoda86@users.noreply.github.com>
2025-02-12 15:21:24 -0700

    Changes dvwa to DVWA in all links and changed pw to password for simplification

--- dvwa-scanner.py
+++ dvwa-scanner.py
@@ -1,12 +1,12 @@
 import scanner
 
-target_url = "http://127.0.0.1/dvwa" #localhost
-links_to_ignore = ["http://127.0.0.1/dvwa/logout.php"]
+target_url = "http://127.0.0.1/DVWA" #localhost
+links_to_ignore = ["http://127.0.0.1/DVWA/logout.php"]
 
-data_dict = {"username" : "admin", "password" : "p@ssword" , "login" : "submit"}
+data_dict = {"username" : "admin", "password" : "password" , "login" : "submit"}
 
 dvwa_scanner = scanner.Scanner(target_url, links_to_ignore)
-dvwa_scanner.session.post("http://127.0.0.1/dvwa/login.php", data=data_dict)
+dvwa_scanner.session.post("http://127.0.0.1/DVWA/login.php", data=data_dict)
 
 dvwa_scanner.crawl()
 dvwa_scanner.run_scanner
