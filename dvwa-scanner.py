b43235396c358e21a7fc1160a0116fdf13633551
dsoda86 <122141740+dsoda86@users.noreply.github.com>
2025-02-13 16:40:34 -0700

    updated urls to be localhost with DVWA to try to access the correct links when trying to run the scanner

--- dvwa-scanner.py
+++ dvwa-scanner.py
@@ -1,12 +1,12 @@
 import scanner
 
-target_url = "http://127.0.0.1/DVWA" #localhost
-links_to_ignore = ["http://127.0.0.1/DVWA/logout.php"]
+target_url = "http://localhost/DVWA/" #localhost
+links_to_ignore = ["http://localhost/DVWA/logout.php"]
 
 data_dict = {"username" : "admin", "password" : "password" , "login" : "submit"}
 
 dvwa_scanner = scanner.Scanner(target_url, links_to_ignore)
-dvwa_scanner.session.post("http://127.0.0.1/DVWA/login.php", data=data_dict)
+dvwa_scanner.session.post("http://localhost/DVWA/login.php", data=data_dict)
 
 dvwa_scanner.crawl()
 dvwa_scanner.run_scanner
